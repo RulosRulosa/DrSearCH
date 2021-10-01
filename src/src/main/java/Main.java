bf93f9e6bb98e25bb882a10e5b6bde9b8bf05973
dankusufro <d.torres17@ufromail.cl>
2021-10-01 00:11:15 -0300

    commit parte1

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -1,31 +1,45 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    //Rulos
-    //Retorna un int[] con los digitos de la fecha ingresada
-    public static int[] CadenaFecha(String ingresoFecha){
-        String[] FechaEnCadena = ingresoFecha.split("/");
-        int[] FechaIntEnCadena = new int[FechaEnCadena.length];
-        for (int i=0;i< FechaEnCadena.length;i++){
-            FechaIntEnCadena[i] = Integer.parseInt(FechaEnCadena[i]);
+    //Danko
+    //Crea un archivo, en este caso crearemos txt
+    public static void crearArchivo(String ruta, String contenido) {
+        Path archivo = Paths.get(ruta);
+        try {
+            Files.write(archivo, contenido.getBytes());
+            System.out.println("El archivo fue creado exitosamente");
+        } catch (IOException e) {
+            System.out.println("El archivo no pudo ser creado");
         }
-        return FechaIntEnCadena;
     }
 
-    //Retorna lo que se escribira en el txt del paciente, el especialista escogido y la fecha agendada
-    public static String EscogerEspecialista(String ruta) {
-        System.out.println("¿Con cual especialista desea agendar hora?");
-        String[] Especialistas = leerArchivo(ruta).split("\n");
-        return Especialistas[Validar(Especialistas.length)]+";"+EscogerFecha();
+    //Retorna un String con el contenido del archivo
+    public static String leerArchivo(String ruta) {
+        Path archivo = Paths.get(ruta);
+        String contenido = "";
+        try {
+            contenido = new String(Files.readAllBytes(archivo));
+        } catch (IOException e) {
+            System.out.println("El archivo no pudo ser leido");
+        }
+        return contenido;
     }
 
-    //Entrada de rut, si este pasa por el validador, se sigue con el codigo
-    public static String IngresarRut() {
-        Scanner teclado = new Scanner(System.in);
-        String rut;
-        do {
-            System.out.println("Ingresar rut:");
-            rut = teclado.next();
-
-        }while (!validarRut(rut));
-        return rut;
+    //Valida los digitos de la fecha ingresada
+    public static boolean fechaValida(String ingresoFecha) {
+        //Guardamos la cadena en otro arreglo para no llamar la funcion tantas veces
+        int[] Cadena = CadenaFecha(ingresoFecha);
+        if (Cadena[0]<1||Cadena[0]>31){
+            return false;
+        }else if (Cadena[1]<1||Cadena[1]>12){
+            return false;
+        }else if (Cadena[2]<2021){
+            return false;
+        }else {
+            return true;
+        }
     }
 }
