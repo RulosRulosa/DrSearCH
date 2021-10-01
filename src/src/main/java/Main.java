016da4accf24653a04939ab82138a079b9852a60
TomasB22 <t.baeza01@ufromail.cl>
2021-10-01 00:36:50 -0300

    Commit Tomás

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -1,87 +1,41 @@
 public class Main {
-    public static void main(String[] args) {
-    Ingreso();
-    }
-    //Tania
-    public static void Ingreso() {
-        //Se guarda el rut que va a ser ingresado por el paciente
-        String RutPaciente = IngresarRut();
-        MostrarPrevisiones();
-        AgendarFecha(IngresarPrevision(), RutPaciente);
+    //Tomás
+    //Imprime por pantalla los especialistas disponibles por prevision escogida
+    public static void MostrarEspecialistasPrevision(String prevision) {
+        System.out.println(leerArchivo(prevision));
     }
 
-    //Se crea un archivo en la carpeta Pacientes, con el rut del paciente, datos del especialista y la fecha agendada
-    public static void AgendarFecha(String ruta, String rut) {
-        if (ruta.equals("Fonasa.txt")) {
-            crearArchivo("Pacientes/" + rut + ".txt", EscogerEspecialista(ruta) + ";Fonasa");
-        } else {
-            crearArchivo("Pacientes/" + rut + ".txt", EscogerEspecialista(ruta) + ";Isapre");
-        }
-        System.out.println("Hora agendada con exito");
-    }
-
-    //Imprime por pantalla las opciones de previsiones
-    public static void MostrarPrevisiones() {
-        System.out.println("Ingresa tu prevision");
-        System.out.println("[1]Fonasa");
-        System.out.println("[2]Isapre");
-    }
-
-    //Ingresa la prevision, luego retorna la ruta de ésta
-    public static String IngresarPrevision() {
-        int Prevision = Validar(2);
-        if (Prevision == 1) {  //Fonasa
-            MostrarEspecialistasPrevision("Fonasa.txt");
-            return "Fonasa.txt";
-        } else {             //Isapre
-            MostrarEspecialistasPrevision("Isapre.txt");
-            return "Isapre.txt";
-        }
-    }
-
-    //Billy
-    //Validador de Rut
-    public static boolean validarRut(String rut) {   // 12.345.678-k
-        boolean valido= false;
-        rut = rut.toUpperCase();            // 12.345.678-K
-        rut = rut.replace("-", "");         // 12.345.678K
-        rut = rut.replace(".", "");         // 12345678k
-        int dv = rut.charAt(rut.length()-1);
-        try{
-            int numRut = Integer.parseInt(rut.substring(0,rut.length()-1));
-
-            int m=0, s=1;
-            for (;numRut !=0;numRut/=10) {
-                s = (s+numRut%10 *(9-m++%6))%11;
-            }
-
-            if(dv==(char)(s!=0 ?s+47:75)){
-                valido = true;
-            }
-
-        }catch(Exception e){
-            System.out.println("rut invalido");
-        }
-
-        return valido;
-    }
-
-    //Valida numeros naturales en un cierto rango, se usa para entrada de opciones
-    public static int Validar(int x) {
-        int n = -1;
+    //Si el ingreso de fecha es válido, lo retorna para guardarlo en su txt
+    public static String EscogerFecha() {
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Date fecha = null;
+        String ingresoFecha;
         do {
-            //Scanner ponerlo dentro del DO, y dentro de una funcion
-            Scanner teclado = new Scanner(System.in);
-            //System.out.println("ingrese otro numero");
-            try {
-                n = teclado.nextInt();
+            ingresoFecha =IngresarFecha();
+            try {                           //Valida formato dd/MM/yyyy
+                fecha = format.parse(ingresoFecha);
             } catch (Exception e) {
-                System.out.println("Error");
+                MostrarFechaIncorrecto();
             }
-            if (n < 0 || n > x) {
-                System.out.println("ingrese un numero valido");
+            if (fechaValida(ingresoFecha)){ //Valida digitos
+                MostrarFechaInvalido();
             }
-        } while (n < 0 || n > x);
-        return n;
+        }while (fecha==null||!fechaValida(ingresoFecha)); //Si el formato o los digitos son invalidos, se sigue en el bucle
+        return ingresoFecha;
+    }
+
+    //Entrada de fecha a agendar
+    public static String IngresarFecha() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresar fecha en formato DD/MM/YYYY");
+        return teclado.next();
+    }
+
+    public static void MostrarFechaInvalido() {
+        System.out.println("Digitos de fecha invalidos");
+    }
+
+    public static void MostrarFechaIncorrecto() {
+        System.out.println("Formato de fecha incorrecto");
     }
 }
