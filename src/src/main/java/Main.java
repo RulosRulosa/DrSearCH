4f16d716b61dffdcfafd016fc30cfecb9b4bbf33
Belén Pinilla <m.pinilla04@ufromail.cl>
2021-10-01 21:28:56 -0300

    arreglo de errores, aún faltan algunos por resolver

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -2,26 +2,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
-   
 public class Main {
     public static void main(String[] args) {
-    Ingreso();
+        Ingreso();
     }
-    //Tania
+
     public static void Ingreso() {
         //Se guarda el rut que va a ser ingresado por el paciente
         String RutPaciente = IngresarRut();
         MostrarPrevisiones();
-        AgendarFecha(IngresarPrevision(), RutPaciente);
+        AgendarHora(IngresarPrevision(),RutPaciente);
+
     }
 
-    //Se crea un archivo en la carpeta Pacientes, con el rut del paciente, datos del especialista y la fecha agendada
-    public static void AgendarFecha(String ruta, String rut) {
-        if (ruta.equals("Fonasa.txt")) {
-            crearArchivo("Pacientes/" + rut + ".txt", EscogerEspecialista(ruta) + ";Fonasa");
-        } else {
-            crearArchivo("Pacientes/" + rut + ".txt", EscogerEspecialista(ruta) + ";Isapre");
+    public static void AgendarHora(String ruta, String rut) {
+        if (ruta.equals("Fonasa.txt")){
+            crearArchivo("Pacientes/"+rut+".txt",EscogerEspecialista(ruta)+";Fonasa");
+        }else {
+            crearArchivo("Pacientes/"+rut+".txt",EscogerEspecialista(ruta)+";Isapre");
         }
         System.out.println("Hora agendada con exito");
     }
@@ -32,20 +35,81 @@ public class Main {
         System.out.println("[1]Fonasa");
         System.out.println("[2]Isapre");
     }
-
     //Ingresa la prevision, luego retorna la ruta de ésta
     public static String IngresarPrevision() {
         int Prevision = Validar(2);
-        if (Prevision == 1) {  //Fonasa
+        if (Prevision==1){  //Fonasa
             MostrarEspecialistasPrevision("Fonasa.txt");
             return "Fonasa.txt";
-        } else {             //Isapre
+        }else {             //Isapre
             MostrarEspecialistasPrevision("Isapre.txt");
             return "Isapre.txt";
         }
     }
+    //Imprime por pantalla los especialistas disponibles por prevision escogida
+    public static void MostrarEspecialistasPrevision(String prevision) {
+        System.out.println(leerArchivo(prevision));
+    }
 
-    //Billy
+
+    public static String EscogerFecha() {
+        Scanner teclado = new Scanner(System.in);
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println("Ingresar fecha en formato DD/MM/YYYY");
+        Date fecha = null;
+        String ingresoFecha = null;
+
+        do {
+            ingresoFecha = teclado.next();
+            try {
+                fecha = format.parse(ingresoFecha);
+            } catch (Exception e) {
+                System.out.println("Formato de fecha incorrecto");
+            }
+        }while (fecha==null&&fechaValida(ingresoFecha));
+        return ingresoFecha;
+    }
+    public static int[] CadenaFecha(String ingresoFecha){
+        String[] FechaEnCadena = ingresoFecha.split("/");
+        int[] FechaIntEnCadena = new int[FechaEnCadena.length];
+        for (int i=0;i< FechaEnCadena.length;i++){
+            FechaIntEnCadena[i] = Integer.parseInt(FechaEnCadena[i]);
+        }
+        return FechaIntEnCadena;
+    }
+    public static boolean fechaValida(String ingresoFecha) {
+        //Guardamos la cadena en otro arreglo para no llamar la funcion tantas veces
+        int[] Cadena = CadenaFecha(ingresoFecha);
+        if (Cadena[0]<1||Cadena[0]>31){
+            return false;
+        }else if (Cadena[1]<1||Cadena[1]>12){
+            return false;
+        }else if (Cadena[2]<2021){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    //Retorna lo que se escribira en el txt del paciente, el especialista escogido y la fecha agendada
+    public static String EscogerEspecialista(String ruta) {
+        System.out.println("¿Con cual especialista desea agendar hora?");
+        String[] Especialistas = leerArchivo(ruta).split("\n");
+        return Especialistas[Validar(Especialistas.length)]+";"+EscogerFecha();
+    }
+
+    //Entrada de rut, si este pasa por el validador, se sigue con el codigo
+    public static String IngresarRut() {
+        Scanner teclado = new Scanner(System.in);
+        String rut;
+        do {
+            System.out.println("Ingresar rut:");
+            rut = teclado.next();
+
+        }while (!validarRut(rut));
+        return rut;
+    }
+
     //Validador de Rut
     public static boolean validarRut(String rut) {   // 12.345.678-k
         boolean valido= false;
@@ -72,28 +136,8 @@ public class Main {
         return valido;
     }
 
-    //Valida numeros naturales en un cierto rango, se usa para entrada de opciones
-    public static int Validar(int x) {
-        int n = -1;
-        do {
-            //Scanner ponerlo dentro del DO, y dentro de una funcion
-            Scanner teclado = new Scanner(System.in);
-            //System.out.println("ingrese otro numero");
-            try {
-                n = teclado.nextInt();
-            } catch (Exception e) {
-                System.out.println("Error");
-            }
-            if (n < 0 || n > x) {
-                System.out.println("ingrese un numero valido");
-            }
-        } while (n < 0 || n > x);
-        return n;
-
-    }
-   //Danko
     //Crea un archivo, en este caso crearemos txt
-   public static void crearArchivo(String ruta, String contenido) {
+    public static void crearArchivo(String ruta, String contenido) {
         Path archivo = Paths.get(ruta);
         try {
             Files.write(archivo, contenido.getBytes());
@@ -115,88 +159,23 @@ public class Main {
         return contenido;
     }
 
-    //Valida los digitos de la fecha ingresada
-    public static boolean fechaValida(String ingresoFecha) {
-        //Guardamos la cadena en otro arreglo para no llamar la funcion tantas veces
-        int[] Cadena = CadenaFecha(ingresoFecha);
-        if (Cadena[0]<1||Cadena[0]>31){
-            return false;
-        }else if (Cadena[1]<1||Cadena[1]>12){
-            return false;
-        }else if (Cadena[2]<2021){
-            return false;
-        }else {
-            return true;
-        }
-      
-      //Rulos
-    //Retorna un int[] con los digitos de la fecha ingresada
-    public static int[] CadenaFecha(String ingresoFecha){
-        String[] FechaEnCadena = ingresoFecha.split("/");
-        int[] FechaIntEnCadena = new int[FechaEnCadena.length];
-        for (int i=0;i< FechaEnCadena.length;i++){
-            FechaIntEnCadena[i] = Integer.parseInt(FechaEnCadena[i]);
-        }
-        return FechaIntEnCadena;
-    }
-
-    //Retorna lo que se escribira en el txt del paciente, el especialista escogido y la fecha agendada
-    public static String EscogerEspecialista(String ruta) {
-        System.out.println("¿Con cual especialista desea agendar hora?");
-        String[] Especialistas = leerArchivo(ruta).split("\n");
-        return Especialistas[Validar(Especialistas.length)]+";"+EscogerFecha();
-    }
-
-    //Entrada de rut, si este pasa por el validador, se sigue con el codigo
-    public static String IngresarRut() {
-        Scanner teclado = new Scanner(System.in);
-        String rut;
+    //Valida numeros naturales en un cierto rango, se usa para entrada de opciones
+    public static int Validar(int x) {
+        int n = -1;
         do {
-            System.out.println("Ingresar rut:");
-            rut = teclado.next();
-
-        }while (!validarRut(rut));
-        return rut;
-    }
-      
-      //Tomás
-    //Imprime por pantalla los especialistas disponibles por prevision escogida
-    public static void MostrarEspecialistasPrevision(String prevision) {
-        System.out.println(leerArchivo(prevision));
-    }
-
-    //Si el ingreso de fecha es válido, lo retorna para guardarlo en su txt
-    public static String EscogerFecha() {
-        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        Date fecha = null;
-        String ingresoFecha;
-        do {
-            ingresoFecha =IngresarFecha();
-            try {                           //Valida formato dd/MM/yyyy
-                fecha = format.parse(ingresoFecha);
+            //Scanner ponerlo dentro del DO, y dentro de una funcion
+            Scanner teclado = new Scanner(System.in);
+            //System.out.println("ingrese otro numero");
+            try {
+                n = teclado.nextInt();
             } catch (Exception e) {
-                MostrarFechaIncorrecto();
+                System.out.println("Error");
             }
-            if (fechaValida(ingresoFecha)){ //Valida digitos
-                MostrarFechaInvalido();
+            if (n < 0 || n > x) {
+                System.out.println("ingrese un numero valido");
             }
-        }while (fecha==null||!fechaValida(ingresoFecha)); //Si el formato o los digitos son invalidos, se sigue en el bucle
-        return ingresoFecha;
-    }
-
-    //Entrada de fecha a agendar
-    public static String IngresarFecha() {
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingresar fecha en formato DD/MM/YYYY");
-        return teclado.next();
-    }
-
-    public static void MostrarFechaInvalido() {
-        System.out.println("Digitos de fecha invalidos");
-    }
-
-    public static void MostrarFechaIncorrecto() {
-        System.out.println("Formato de fecha incorrecto");
+        } while (n < 0 || n > x);
+        return n;
     }
 }
 
