2e14dfd073aa9b267be90fa66b4e20f3f9ae777c
TomasB22 <81914981+TomasB22@users.noreply.github.com>
2021-10-31 19:05:44 -0300

    Add files via upload

---
+++ src/test/java/EspecialistaTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class EspecialistaTest {
+    Especialista e;
+//
+//    @BeforeEach
+//    void setUp() {
+//        e= new Especialista(1,"Juan ");
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//        p = null;
+//    }
+
+    @Test
+    void mostrarHoras() {
+
+        switch(LocalDate.now().getDayOfWeek()){
+            case FRIDAY:
+                System.out.println("ESCHAVAAALEEEE QUE E VIERNEE!!!!");
+            case MONDAY:
+        }
+    }
+
+//    @Test
+//    void mostrar(){
+//       Agenda.show();
+//    }
+}
